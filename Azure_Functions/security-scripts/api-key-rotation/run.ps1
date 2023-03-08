c6e9a72897137830240ecab30ff0ade0d202c33f
mernaAlghannam <46355198+mernaAlghannam@users.noreply.github.com>
2023-03-08 00:07:14 -0500

    added temp simple rotate key script (must add more things later that updates env variable)

---
+++ Azure_Functions/security-scripts/api-key-rotation/run.ps1
@@ -0,0 +1,25 @@
+# Input bindings are passed in via param block.
+param($Timer)
+
+# Get the current universal time in the default string format.
+$currentUTCtime = (Get-Date).ToUniversalTime()
+
+# The 'IsPastDue' property is 'true' when the current function invocation is later than scheduled.
+if ($Timer.IsPastDue) {
+    Write-Host "PowerShell timer is running late!"
+}
+
+# Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
+
+Get-Module -Name Az -ListAvailable
+Import-Module Az
+
+az functionapp function keys set -g warehouse-automation_group -n shipping-data-api --function-name update-shipping-data --key-name default
+
+
+az functionapp function keys set -g warehouse-automation_group -n shipping-data-api --function-name get-shipping-data --key-name default
+
+
+# Write an information log with the current time.
+Write-Host "PowerShell timer trigger function ran! TIME: $currentUTCtime"
+
