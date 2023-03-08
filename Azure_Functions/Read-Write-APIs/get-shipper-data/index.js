c71a5837547c4c921226e0cedf0e8f543163c8a5
mernaAlghannam <46355198+mernaAlghannam@users.noreply.github.com>
2023-03-07 20:25:39 -0500

    added read and write API Scripts

--- Azure_Functions/Read-Write-APIs/get-shipper-data/index.js
+++ Azure_Functions/Read-Write-APIs/get-shipper-data/index.js
@@ -1,6 +1,5 @@
 module.exports = async function (context, req) {
   var documents = context.bindings.documents;
-  context.log('Shippers complete information '+ JSON.stringify(documents));
 
   context.res = {
       // status: 200, /* Defaults to 200 */
