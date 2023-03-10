93fc33cc745081fee259f81549a9b0e547cca494
mernaAlghannam <46355198+mernaAlghannam@users.noreply.github.com>
2023-03-09 23:14:58 -0500

    changed title of site

--- src/globals.ts
+++ src/globals.ts
@@ -3,10 +3,7 @@
  * We've put a few here that we know you will need.
  * Fill in the blank for each one
  */
-export const MY_BU_ID = "U45003819";
 export const BASE_API_URL = "https://dscs519-assessment.azurewebsites.net/api";
-// You can get this from Piazza
-export const TOKEN = "fKZTwhwT1DV64q_JzG6sYoShfq-cJbPwBgjIMOImYSTiAzFuv4-H5g==";
 // This is a helper function to generate the headers with the x-functions-key attached
 export const GET_DEFAULT_HEADERS = () => {
   var headers = new Headers();
