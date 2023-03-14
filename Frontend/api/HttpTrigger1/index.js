12c277193f5184d0872f15ce6aba07c45af302bb
mernaAlghannam <46355198+mernaAlghannam@users.noreply.github.com>
2023-03-14 04:34:12 -0400

    .

--- Frontend/api/HttpTrigger1/index.js
+++ Frontend/api/HttpTrigger1/index.js
@@ -1,17 +1,11 @@
 const https = require("https"); // 1
-
-const options = {
-    hostname: 'shipping-data-api.azurewebsites.net',
-    path: '/api/get-shipping-data',
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'x-functions-key': ""+process.env.API_KEY
-    },
-  };
   
 // Sample URL
-const url = 'https://shipping-data-api.azurewebsites.net/api/get-shipping-data?code='+process.env.API_KEY;
+
+module.exports = async function (context, req) {
+
+
+    const url = 'https://shipping-data-api.azurewebsites.net/api/get-shipping-data?code='+process.env.API_KEY;
   
 const request = https.request(url, (response) => {
     let data = '';
@@ -30,11 +24,6 @@ request.on('error', (error) => {
 });
   
 request.end() 
-
-module.exports = async function (context, req) {
-
-
-
 
     // const url = 'https://shipping-data-api.azurewebsites.net/api/get-shipping-data';
     // const headers = {
