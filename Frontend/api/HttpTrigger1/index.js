c170a03d7712a4e2bf3356dd8fca707d732abed3
mernaAlghannam <46355198+mernaAlghannam@users.noreply.github.com>
2023-03-14 03:47:26 -0400

    .

--- Frontend/api/HttpTrigger1/index.js
+++ Frontend/api/HttpTrigger1/index.js
@@ -8,8 +8,13 @@ module.exports = async function (context, req) {
         'x-functions-key': ""+process.env.API_KEY
     };
 
-    await fetch(url, { headers }) // 3
-        .then(response => response.json())
-        .then(response => context.res.json(response)); // 4
+    const response = await fetch(url, { headers });
+    const body = await response.json();
+
+    // await fetch(url, { headers }) // 3
+    //     .then(response => response.json())
+    //     .then(response => context.res.json(response)); // 4
+
+    context.res.json(response)
 
 }
