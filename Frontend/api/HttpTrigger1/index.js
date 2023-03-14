5d87ff988b7b88f4e01f48fe273195afb7c210d8
mernaAlghannam <46355198+mernaAlghannam@users.noreply.github.com>
2023-03-14 04:54:18 -0400

    .

--- Frontend/api/HttpTrigger1/index.js
+++ Frontend/api/HttpTrigger1/index.js
@@ -1,31 +1,51 @@
 const https = require("https"); // 1
   
-// Sample URL
+const url = 'https://shipping-data-api.azurewebsites.net/api/get-shipping-data?code='+process.env.API_KEY;
+const responsebody = {}
+
+const request = https.request(url, (response) => {
+    let data = '';
+    response.on('data', (chunk) => {
+        data = data + chunk.toString();
+    });
+
+    response.on('end', () => {
+        const body = JSON.parse(data);
+        responsebody = body;
+    });
+})
+
+const error = "an error"
+request.on('error', (error) => {
+    error = error;
+});
+
+request.end()
 
 module.exports = async function (context, req) {
 
 
-    const url = 'https://shipping-data-api.azurewebsites.net/api/get-shipping-data?code='+process.env.API_KEY;
-    const responsebody = {}
+    // const url = 'https://shipping-data-api.azurewebsites.net/api/get-shipping-data?code='+process.env.API_KEY;
+    // const responsebody = {}
   
-    const request = await https.request(url, (response) => {
-        let data = '';
-        response.on('data', (chunk) => {
-            data = data + chunk.toString();
-        });
+    // const request = await https.request(url, (response) => {
+    //     let data = '';
+    //     response.on('data', (chunk) => {
+    //         data = data + chunk.toString();
+    //     });
     
-        response.on('end', () => {
-            const body = JSON.parse(data);
-            responsebody = body;
-        });
-    })
+    //     response.on('end', () => {
+    //         const body = JSON.parse(data);
+    //         responsebody = body;
+    //     });
+    // })
     
-    const error = "an error"
-    await request.on('error', (error) => {
-        error = error;
-    });
+    // const error = "an error"
+    // await request.on('error', (error) => {
+    //     error = error;
+    // });
     
-    await request.end() 
+    // await request.end() 
 
     context.res.json({error: error});
 
