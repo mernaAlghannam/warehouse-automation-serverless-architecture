5e279a690bf36f913841271c1aec4b0e6177bc85
mernaAlghannam <46355198+mernaAlghannam@users.noreply.github.com>
2023-03-14 05:07:16 -0400

    .

--- Frontend/api/HttpTrigger1/index.js
+++ Frontend/api/HttpTrigger1/index.js
@@ -1,38 +1,59 @@
 const https = require("https"); // 1
-  
+
 const url = 'https://shipping-data-api.azurewebsites.net/api/get-shipping-data?code='+process.env.API_KEY;
-const responsebody = {}
 
-const request = https.request(url, (response) => {
-    let data = '';
-    response.on('data', (chunk) => {
-        data = data + chunk.toString();
+const options = {
+    hostname: 'shipping-data-api.azurewebsites.net',
+    path: '/api/get-shipping-data',
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-functions-key': ""+process.env.API_KEY
+    },
+  };
+
+/**
+ * Do a request with options provided.
+ *
+ * @param {Object} options
+ * @return {Promise} a promise of request
+ */
+function doRequest(options, data) {
+    return new Promise((resolve, reject) => {
+      const req = https.request(options, (res) => {
+        res.setEncoding('utf8');
+        let responseBody = '';
+  
+        res.on('data', (chunk) => {
+          responseBody += chunk;
+        });
+  
+        res.on('end', () => {
+          resolve(JSON.parse(responseBody));
+        });
+      });
+  
+      req.on('error', (err) => {
+        reject(err);
+      });
+  
+      req.end();
+      
     });
-
-    response.on('end', () => {
-        const body = JSON.parse(data);
-        responsebody = body;
-    });
-})
-
-const error = "an error"
-request.on('error', (error) => {
-    error = error;
-});
-
-request.end()
+  }
+  
 
 module.exports = async function (context, req) {
 
+    await doRequest(url);
 
-    // const url = 'https://shipping-data-api.azurewebsites.net/api/get-shipping-data?code='+process.env.API_KEY;
     // const responsebody = {}
   
     // const request = await https.request(url, (response) => {
-    //     let data = '';
-    //     response.on('data', (chunk) => {
-    //         data = data + chunk.toString();
-    //     });
+        // let data = '';
+        // response.on('data', (chunk) => {
+        //     data = data + chunk.toString();
+        // });
     
     //     response.on('end', () => {
     //         const body = JSON.parse(data);
@@ -47,6 +68,6 @@ module.exports = async function (context, req) {
     
     // await request.end() 
 
-    context.res.json({error: error});
+    context.res.json({error: "here"});
 
 }
