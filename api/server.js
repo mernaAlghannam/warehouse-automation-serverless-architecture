b3a391d28ef259d1fc04b56e424cf024554d1014
mernaAlghannam <46355198+mernaAlghannam@users.noreply.github.com>
2023-03-13 22:42:38 -0400

    added env variable to app settings and called it here

--- api/server.js
+++ api/server.js
@@ -8,37 +8,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// const options = {
-//   hostname: 'shipping-data-api.azurewebsites.net',
-//   port: 443,
-//   path: '/api/get-shipping-data',
-//   method: 'GET',
-//   headers: {
-//   'accept': 'application/json',
-//   'x-functions-key': 
-//   },
-// };
-
-// const req = https.request(options, res => {
-//   console.log(`statusCode: ${res.statusCode}`);
-
-//   res.on('data', d => {
-//     process.stdout.write(d);
-//   });
-// });
-
-// req.on('error', error => {
-//   console.error(error);
-// });
-
 
 app.get('/', function(req, res, next) {
   request({
-    uri: 'https://shipping-data-api.azurewebsites.net/api/get-shipping-data?code='+process.env.REACT_APP_NOT_SECRET_CODE
-    // qs: {
-    //   api_: 'wbZSAG9uF_QA_fFqaJOrzlMYmf_pV1oUqNX5fRuh9TXbAzFukRXm7g==',
-    //   // query: 'World of Warcraft: Legion'
-    // }
+    headers: {'x-functions-key':''+process.env.REACT_APP_NOT_SECRET_CODE},
+    uri: 'https://shipping-data-api.azurewebsites.net/api/get-shipping-data'
   }).pipe(res);
 });
 
