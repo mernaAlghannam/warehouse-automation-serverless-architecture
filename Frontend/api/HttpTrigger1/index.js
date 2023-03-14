1ef9043569df8ac87d3b9587469b1efc692eb531
mernaAlghannam <46355198+mernaAlghannam@users.noreply.github.com>
2023-03-14 04:45:09 -0400

    .

--- Frontend/api/HttpTrigger1/index.js
+++ Frontend/api/HttpTrigger1/index.js
@@ -27,43 +27,6 @@ module.exports = async function (context, req) {
     
     request.end() 
 
-    context.res.json(responsebody);
-
-    // const url = 'https://shipping-data-api.azurewebsites.net/api/get-shipping-data';
-    // const headers = {
-    //     'accept': 'application/json',
-    //     'x-functions-key': ""+process.env.API_KEY
-    // };
-
-//     try {
-
-//     const response = await fetch("https://shipping-data-api.azurewebsites.net/api/get-shipping-data", {
-//         method: "GET",
-//         headers: {
-//           'accept': 'application/json',
-//           'x-functions-key': ""+process.env.API_KEY
-//         },
-//       }
-//       );
-
-//     const body = await response.json();
-//     context.res.json(body)
-
-// } catch (error) {
-//     if (error.name === 'AbortError') {
-//         context.res.json({error: 'request was aborted'});
-//     }else{
-//     context.res.json({error: 'request was aborted'});
-//     }
-// }
-
-
-
-
-// context.res.json({error: 'request was aborted'});
-
-    // await fetch(url, { headers }) // 3
-    //     .then(response => response.json())
-    //     .then(response => context.res.json(response)); // 4
+    context.res.json(error);
 
 }
