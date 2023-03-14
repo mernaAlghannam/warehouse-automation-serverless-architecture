5db1da035e47ffdb06dc2643fd941c049137b292
mernaAlghannam <46355198+mernaAlghannam@users.noreply.github.com>
2023-03-14 03:56:58 -0400

    .

--- Frontend/api/HttpTrigger1/index.js
+++ Frontend/api/HttpTrigger1/index.js
@@ -2,19 +2,27 @@ const fetch = require("node-fetch"); // 1
 
 module.exports = async function (context, req) {
 
-    const url = 'https://shipping-data-api.azurewebsites.net/api/get-shipping-data';
-    const headers = {
-        'accept': 'application/json',
-        'x-functions-key': ""+process.env.API_KEY
-    };
+    // const url = 'https://shipping-data-api.azurewebsites.net/api/get-shipping-data';
+    // const headers = {
+    //     'accept': 'application/json',
+    //     'x-functions-key': ""+process.env.API_KEY
+    // };
 
-    const response = await fetch(url, {method: 'GET', headers });
+    const response = await fetch("https://shipping-data-api.azurewebsites.net/api/get-shipping-data", {
+        method: "GET",
+        headers: {
+          'accept': 'application/json',
+          'x-functions-key': ""+process.env.API_KEY
+        },
+      }
+      );
+
     const body = await response.json();
 
     // await fetch(url, { headers }) // 3
     //     .then(response => response.json())
     //     .then(response => context.res.json(response)); // 4
 
-    context.res.json(response)
+    context.res.json(body)
 
 }
