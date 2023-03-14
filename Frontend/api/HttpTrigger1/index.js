dab22d701bf718ba7da42ec08d2169d24d2254fe
mernaAlghannam <46355198+mernaAlghannam@users.noreply.github.com>
2023-03-14 04:29:06 -0400

    .

--- Frontend/api/HttpTrigger1/index.js
+++ Frontend/api/HttpTrigger1/index.js
@@ -1,16 +1,40 @@
 const https = require("https"); // 1
 
+const options = {
+    hostname: 'shipping-data-api.azurewebsites.net',
+    path: '/api/get-shipping-data',
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-functions-key': ""+process.env.API_KEY
+    },
+  };
+  
+// Sample URL
+const url = 'https://shipping-data-api.azurewebsites.net/api/get-shipping-data?code='+process.env.API_KEY;
+  
+const request = https.request(url, (response) => {
+    let data = '';
+    response.on('data', (chunk) => {
+        data = data + chunk.toString();
+    });
+  
+    response.on('end', () => {
+        const body = JSON.parse(data);
+        console.log(body);
+    });
+})
+  
+request.on('error', (error) => {
+    console.log('An error', error);
+});
+  
+request.end() 
+
 module.exports = async function (context, req) {
 
-    const options = {
-        hostname: 'shipping-data-api.azurewebsites.net',
-        path: '/api/get-shipping-data',
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          'x-functions-key': ""+process.env.API_KEY
-        },
-      };
+
+
 
     // const url = 'https://shipping-data-api.azurewebsites.net/api/get-shipping-data';
     // const headers = {
