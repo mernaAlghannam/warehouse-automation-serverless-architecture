c7562c9f3f124a12a69753d1d094fef54787fc47
mernaAlghannam <46355198+mernaAlghannam@users.noreply.github.com>
2023-03-14 21:46:58 -0400

    .

--- security-scripts/api-key-rotation/run.ps1
+++ security-scripts/api-key-rotation/run.ps1
@@ -3,12 +3,13 @@ az login
 
 az account set --subscription 222777e5-63af-4bcd-9b37-938e8cd5b92e
 
-# az functionapp function keys set -g warehouse-automation_group -n shipping-data-api --function-name update-shipping-data --key-name default
+az functionapp function keys set -g warehouse-automation_group -n shipping-data-api --function-name update-shipping-data --key-name default
 
 
 $newapikey = az functionapp function keys set -g warehouse-automation_group -n shipping-data-api --function-name get-shipping-data --key-name default | Out-String | ConvertFrom-Json 
 
 $newapikey = $newapikey.value
+
 az account set --subscription 222777e5-63af-4bcd-9b37-938e8cd5b92e
 
 az staticwebapp appsettings set --name warehouse-automator --setting-names "API_KEY=$newapikey"
